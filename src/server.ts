ad116dac3becca32357ca42795874122f8e73fd7
Thiago Cedran <thiagocdn@gmail.com>
2025-07-13 17:08:25 -0300

    feat: added new route to create a room

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
 import { env } from './env.ts';
+import { createRoomRoute } from './http/routes/create-room.ts';
 import { getRoomsRoute } from './http/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -22,6 +23,7 @@ app.get('/health', () => {
 });
 
 app.register(getRoomsRoute);
+app.register(createRoomRoute);
 
 app.listen({ port: env.PORT }).then(() => {
   // biome-ignore lint/suspicious/noConsole: just a verification log
